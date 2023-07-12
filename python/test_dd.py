9543beaf7d4a8b429e34f8aa8516512d776086f5
Builderlink <abcsky23@gmail.com>
2023-07-12 12:25:04 -0700

    polish the personal bio

--- python/test_dd.py
+++ python/test_dd.py
@@ -1,1 +1,7 @@
-print('hello world')
+with open('ai_sync_yc_2023.txt', 'r') as file:
+    lines = file.readlines()
+
+new_lines = [line + "\n" for line in lines if 'You' not in line]
+
+with open('ai_sync_yc_2023.txt', 'w') as file:
+    file.writelines(new_lines)
